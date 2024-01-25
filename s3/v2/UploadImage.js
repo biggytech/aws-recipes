59e35192c91817ded8edcaea3a0f8cd7d09095c4
Neli Harbuzava <Neli.Harbuzava@ventionteams.com>
2024-01-25 22:28:45 +0300

    s3 v2 - putObject() vs upload()

--- s3/v2/UploadImage.js
+++ s3/v2/UploadImage.js
@@ -13,6 +13,7 @@ const uploadImage = (fileName) => {
         Body: fileContent
     };
 
+    // putObject() for smaller objects
     s3.putObject(params, (err, data) => {
         if (err) {
             console.log(err);
@@ -20,6 +21,17 @@ const uploadImage = (fileName) => {
             console.log(data);
         }
     });
+
+    // OR upload() for bigger objects
+    // s3.upload(params, (err, data) => {
+    //     if (err) {
+    //         console.log(err);
+    //     } else {
+    //         console.log(data);
+    //     }
+    // });
+
+    // See: https://stackoverflow.com/questions/38442512/difference-between-upload-and-putobject-for-uploading-a-file-to-s3
 };
 
 uploadImage('data/image.png');
