131db05ba23659d7914fde4a099f8923d7f47a2a
Neli Harbuzava <ineliharbuzava@gmail.com>
2024-03-03 13:59:43 +0300

    s3 v3 - create bucket

---
+++ s3/v3/CreateBucket.js
@@ -0,0 +1,17 @@
+import { S3Client, CreateBucketCommand } from '@aws-sdk/client-s3'
+
+const s3Client = new S3Client();
+
+const run = async () => {
+    try {
+        const data = await s3Client.send(new CreateBucketCommand({
+            Bucket: 'neli',
+            ACL: 'private'
+        }));
+        console.log('Success', data);
+    } catch (err) {
+        console.log('Error', err);
+    }
+};
+
+run();
