a76640af4299ed4730eb7d75d2356273389c2e55
Neli Harbuzava <ineliharbuzava@gmail.com>
2024-05-10 20:54:38 +0300

    rds v2 postgresql - update data

---
+++ rds/v2/postgresql/UpdateData.js
@@ -0,0 +1,32 @@
+const { Client } = require('pg');
+
+const client = new Client({
+    host: 'postgredb.cjxjvqjxjxjx.us-east-1.rds.amazonaws.com',
+    user: 'postgres',
+    password: 'password',
+    port: 5432,
+    database: 'pgdatabase',
+});
+
+const updateData = () => {
+    client.connect();
+    console.log('Connected to PostgreSQL');
+
+    const query = `
+        UPDATE Employee
+        SET EMAIL = 'updated@gmail.com'
+        WHERE ID = 1;
+    `;
+
+    client.query(query, (err, res) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Data updated');
+            console.log(res.rows);
+        }
+        client.end();
+    });
+};
+
+updateData();
