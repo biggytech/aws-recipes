cf7f0a7f452c3d1d1bd5dfe646f1a091a699b894
Neli Harbuzava <ineliharbuzava@gmail.com>
2024-05-18 21:40:42 +0300

    ec2 v2 - stop instance

---
+++ ec2/v2/StopInstance.js
@@ -0,0 +1,15 @@
+const AWS = require('aws-sdk');
+
+const ec2 = new AWS.EC2();
+
+ec2.stopInstances({
+    InstanceIds: [
+        'i-064c3011f90c7e4c8'
+    ]
+}, (err, data) => {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(data);
+    }
+});
