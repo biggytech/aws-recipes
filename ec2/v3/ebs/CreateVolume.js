1138a8a0ae7c63cbdb4c03da2dace2ea4efeed52
Neli Harbuzava <ineliharbuzava@gmail.com>
2024-06-02 18:54:11 +0300

    ec2 v3 - create ebs volume

---
+++ ec2/v3/ebs/CreateVolume.js
@@ -0,0 +1,20 @@
+import { EC2Client, CreateVolumeCommand } from '@aws-sdk/client-ec2';
+
+// EBS - Elastic Block Storage (SSDs/HDDs for EC2 instances)
+
+const ec2Client = new EC2Client();
+
+const run = async () => {
+    try {
+        const data = await ec2Client.send(new CreateVolumeCommand({
+            AvailabilityZone: 'us-east-1d', // important! should be the same as in the EC2 instance
+            Size: 5, // GB
+            VolumeType: 'gp2', // General Purpose SSD (free tier)
+        }));
+        console.log('Volume created', data);
+    } catch (err) {
+        console.error(err, err.stack);
+    }
+};
+
+run();
