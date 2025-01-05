590ebdd66ee00e6d876309a8a721b9e416789232
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-01-05 19:56:52 +0300

    lambda - create IAM role

---
+++ lambda/v3/CreateIAMRole.js
@@ -0,0 +1,33 @@
+import { IAMClient, CreateRoleCommand } from "@aws-sdk/client-iam";
+
+const iamClient = new IAMClient();
+
+const role_policy = {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "lambda.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+};
+
+const params = {
+    AssumeRolePolicyDocument: JSON.stringify(role_policy),
+    RoleName: "lambda-role"
+};
+
+const run = async () => {
+    try {
+        const data = await iamClient.send(new CreateRoleCommand(params));
+        console.log('Role is created', data);
+    } catch (err) {
+        console.error('Unable to create role', err);
+    }
+};
+
+exports.handler = run;
