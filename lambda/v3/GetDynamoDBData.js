279d99074433bf7909dcbb56a3e662d7098c094d
Neli Harbuzava <ineliharbuzava@gmail.com>
2025-01-05 19:04:35 +0300

    lambda - get dynamodb data

---
+++ lambda/v3/GetDynamoDBData.js
@@ -0,0 +1,25 @@
+const { DynamoDBClient, GetItemCommand } = require('@aws-sdk/client-dynamodb');
+/**
+ * ^ for v3 upload folder with node_modules and package.json
+ * to lambda function
+ */
+
+const ddClient = new DynamoDBClient();
+
+const params = {
+    TableName: 'Employee',
+    Key: {
+        'id': { N: '1' }
+    }
+};
+
+exports.handler = async (event) => {
+    try {
+        const data = await ddClient.send(new GetItemCommand(params));
+        const items = data.Item;
+
+        console.log(items);
+    } catch (err) {
+        console.error(err);
+    }
+};
